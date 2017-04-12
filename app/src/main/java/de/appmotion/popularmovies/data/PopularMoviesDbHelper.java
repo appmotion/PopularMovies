134192f13e9ec6afc576aa76c637c3125047f795
thimo <thimo@appmotion.de>
2017-04-12 17:36:47 +0200

    format code

--- app/src/main/java/de/appmotion/popularmovies/data/PopularMoviesDbHelper.java
+++ app/src/main/java/de/appmotion/popularmovies/data/PopularMoviesDbHelper.java
@@ -6,38 +6,42 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class PopularMoviesDbHelper extends SQLiteOpenHelper {
 
-    // The database name
-    private static final String DATABASE_NAME = "popularmovies.db";
+  // The database name
+  private static final String DATABASE_NAME = "popularmovies.db";
 
-    // If you change the database schema, you must increment the database version
-    private static final int DATABASE_VERSION = 1;
+  // If you change the database schema, you must increment the database version
+  private static final int DATABASE_VERSION = 1;
 
-    // Constructor
-    public PopularMoviesDbHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
+  // Constructor
+  public PopularMoviesDbHelper(Context context) {
+    super(context, DATABASE_NAME, null, DATABASE_VERSION);
+  }
 
-    @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+  @Override public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
-        // Create a table to hold Favoritelist data
-        final String SQL_CREATE_WAITLIST_TABLE = "CREATE TABLE " + PopularMoviesContract.FavoritelistEntry.TABLE_NAME + " (" +
-            PopularMoviesContract.FavoritelistEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-            PopularMoviesContract.FavoritelistEntry.COLUMN_MOVIE_ID + " LONG NOT NULL UNIQUE, " +
-            PopularMoviesContract.FavoritelistEntry.COLUMN_MOVIE_NAME + " TEXT NOT NULL, " +
-            PopularMoviesContract.FavoritelistEntry.COLUMN_TIMESTAMP + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
-                "); ";
+    // Create a table to hold Favoritelist data
+    final String SQL_CREATE_WAITLIST_TABLE = "CREATE TABLE "
+        + PopularMoviesContract.FavoritelistEntry.TABLE_NAME
+        + " ("
+        + PopularMoviesContract.FavoritelistEntry._ID
+        + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+        + PopularMoviesContract.FavoritelistEntry.COLUMN_MOVIE_ID
+        + " LONG NOT NULL UNIQUE, "
+        + PopularMoviesContract.FavoritelistEntry.COLUMN_MOVIE_NAME
+        + " TEXT NOT NULL, "
+        + PopularMoviesContract.FavoritelistEntry.COLUMN_TIMESTAMP
+        + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
+        + "); ";
 
-        sqLiteDatabase.execSQL(SQL_CREATE_WAITLIST_TABLE);
-    }
+    sqLiteDatabase.execSQL(SQL_CREATE_WAITLIST_TABLE);
+  }
 
-    @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        // For now simply drop the table and create a new one. This means if you change the
-        // DATABASE_VERSION the table will be dropped.
-        // In a production app, this method might be modified to ALTER the table
-        // instead of dropping it, so that existing data is not deleted.
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + PopularMoviesContract.FavoritelistEntry.TABLE_NAME);
-        onCreate(sqLiteDatabase);
-    }
+  @Override public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    // For now simply drop the table and create a new one. This means if you change the
+    // DATABASE_VERSION the table will be dropped.
+    // In a production app, this method might be modified to ALTER the table
+    // instead of dropping it, so that existing data is not deleted.
+    sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + PopularMoviesContract.FavoritelistEntry.TABLE_NAME);
+    onCreate(sqLiteDatabase);
+  }
 }
--- app/src/test/java/de/appmotion/popularmovies/ContractClassUnitTest.java
+++ app/src/test/java/de/appmotion/popularmovies/ContractClassUnitTest.java
@@ -16,14 +16,12 @@ import static org.junit.Assert.assertTrue;
  */
 public class ContractClassUnitTest {
 
-  @Test
-  public void inner_class_exists() throws Exception {
+  @Test public void inner_class_exists() throws Exception {
     Class[] innerClasses = PopularMoviesContract.class.getDeclaredClasses();
     assertEquals("There should be 1 Inner class inside the contract class", 1, innerClasses.length);
   }
 
-  @Test
-  public void inner_class_type_correct() throws Exception {
+  @Test public void inner_class_type_correct() throws Exception {
     Class[] innerClasses = PopularMoviesContract.class.getDeclaredClasses();
     assertEquals("Cannot find inner class to complete unit test", 1, innerClasses.length);
     Class entryClass = innerClasses[0];
@@ -32,18 +30,16 @@ public class ContractClassUnitTest {
     assertTrue("Inner class should be static", Modifier.isStatic(entryClass.getModifiers()));
   }
 
-  @Test
-  public void inner_class_members_correct() throws Exception {
+  @Test public void inner_class_members_correct() throws Exception {
     Class[] innerClasses = PopularMoviesContract.class.getDeclaredClasses();
     assertEquals("Cannot find inner class to complete unit test", 1, innerClasses.length);
     Class entryClass = innerClasses[0];
     Field[] allFields = entryClass.getDeclaredFields();
     assertEquals("There should be exactly 4 String members in the inner class", 4, allFields.length);
     for (Field field : allFields) {
-      assertTrue("All members in the contract class should be Strings", field.getType()==String.class);
+      assertTrue("All members in the contract class should be Strings", field.getType() == String.class);
       assertTrue("All members in the contract class should be final", Modifier.isFinal(field.getModifiers()));
       assertTrue("All members in the contract class should be static", Modifier.isStatic(field.getModifiers()));
     }
   }
-
 }
